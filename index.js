418033ec555918fcf85dd18bd3dc4e6d0556945c
Troy Whiteley <dawnerd@users.noreply.github.com>
2016-05-06 12:37:32 -0700

    Update to use new airbnb config (#8)
    * Update to use new airbnb config
    
    * Feedback updates

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-'use strict';
-
 module.exports = {
   extends: [
-    'eslint-config-airbnb/base',
-    'eslint-config-airbnb/rules/strict'
+    'airbnb'
   ],
   ecmaFeatures: {
-    'modules': false
+    modules: false
   },
   rules: {
     'comma-dangle': 0,
-    'strict': [2, 'global'],
+    strict: 0,
     'func-names': 0,
     'vars-on-top': 0,
     'spaced-comment': 0,
     'no-param-reassign': 0,
     'space-before-function-paren': 0,
     'consistent-return': 0,
-    'max-len': 0
+    'max-len': 0,
+    'callback-return': [2, ['callback', 'cb', 'next', 'done', 'allDone']]
   }
 };
